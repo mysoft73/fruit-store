634153e8e96ede4cc706c34491fa8d97e8fff523
mysoft73 <ablert.ks73@gmail.com>
2019-02-20 01:46:49 +0300

    render fruits info fetched form api

--- app/assets/javascripts/components/_all_fruits.js.jsx
+++ app/assets/javascripts/components/_all_fruits.js.jsx
@@ -11,11 +11,21 @@ class AllFruits extends React.Component {
       .then((response) => {return response.json()})
       .then((data) => {this.setState({ fruits: data }) });
   }
+
   render(){
-    return(
-      <div>
-        <h1>To do: List of fruits</h1>
+    var fruits = this.state.fruits.map((fruit) => {
+     return(
+      <div key={fruit.id}>
+       <h1>{fruit.name}</h1>
+       <p>{fruit.description}</p>
       </div>
      )
-   }
+    })
+
+    return(
+     <div>
+      {fruits}
+     </div>
+    )
+  }
 }
